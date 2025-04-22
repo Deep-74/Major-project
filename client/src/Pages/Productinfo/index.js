724cfe80f1ab27b3852bff98541d95245215b933
Deep-74 <96618661+Deep-74@users.noreply.github.com>
2025-04-22 18:33:48 +0530

    Update index.js

--- client/src/Pages/Productinfo/index.js
+++ client/src/Pages/Productinfo/index.js
@@ -299,7 +299,7 @@ function ProductInfo() {
                       </div>
                       <div className="flex justify-between text-gray-600">
                         <span>Bid Amount</span>
-                        <span> $ {bid.bidAmount}</span>
+                        <span> {bid.bidAmount}  <i class="ri-money-rupee-circle-line "></i></span>
                       </div>
                       <div className="flex justify-between text-gray-600">
                         <span>Bid Place On</span>
